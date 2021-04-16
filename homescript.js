431760c39feb4a8c2204df9e95644e81aafc3f08
bdiaz01 <Ryan@Ryans-MacBook-Pro-2.local>
2021-04-16 17:54:53 -0400

    Javascript Background on Hover

--- homescript.js
+++ homescript.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     console.log("oh my god oh my god");
+    $("#ocean").hover(backgroundToggle);
+
+    function backgroundToggle() {
+        $("#backgroundHover").toggleClass("show");
+    }
 });
