94c3db3fc6b0b081e3872fe2cd5a1aec57febeae
bdiaz01 <Ryan@Ryans-MacBook-Pro-2.local>
2021-04-17 12:49:25 -0400

    Updated JS Background on Hover & type leading

--- homescript.js
+++ homescript.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
     console.log("oh my god oh my god");
-    $("#ocean").hover(backgroundToggle);
+    $(".cell").hover(backgroundToggle);
 
     function backgroundToggle() {
+        var thisCell = $(this);
+        var thisID = thisCell.attr('id');
+        console.log(thisID);
+        $("#backgroundHover").css({
+            "background" : `url(home-assets/${thisID}.jpg)`,
+            "background-repeat" : "no-repeat",
+            "background-position" : "center",
+            "background-size" : "auto 50vh"
+        });
         $("#backgroundHover").toggleClass("show");
     }
 });
